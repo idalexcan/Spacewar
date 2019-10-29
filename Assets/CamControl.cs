8a4840daf31c4d1f6fc9d41453bc437c9b75d2e0
Alexis Cano <id.alexcan@gmail.com>
2019-10-29 11:13:51 -0500

    Initial commit

---
+++ Assets/CamControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControl : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Heroship.Move(gameObject,0.5f);
+    }
+}
---
+++ Assets/Heroship.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heroship : MonoBehaviour
+{
+    GameObject ship;
+    GameObject cam;
+    public static float speed=1;
+    void Awake()
+    {
+        ship=transform.GetChild(0).gameObject;
+        cam=transform.GetChild(1).gameObject;
+    }
+    void Start()
+    {
+        
+    }
+
+    public static float xrot, yrot;
+    //Vector3 rot;
+    void Update()
+    {
+        Move(gameObject,1);
+    }
+
+    public static void Move(GameObject go, float sensibility)
+    {
+        xrot=Input.GetAxis("Mouse X");
+        yrot=Input.GetAxis("Mouse Y");
+        go.transform.eulerAngles+=new Vector3(yrot,xrot,0)*sensibility;
+        go.transform.position+=new Vector3();
+        if(Input.GetKey(KeyCode.W))
+        {
+            go.transform.position+=go.transform.forward*speed;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            go.transform.position-=go.transform.forward*speed;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            go.transform.position+=go.transform.right*speed;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            go.transform.position-=go.transform.right*speed;
+        }
+    }
+    
+}
