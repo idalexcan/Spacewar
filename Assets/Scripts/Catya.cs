8ee52512cdf45c67071dcd710e9eec4c71dcd13d
idalexcan <id.alexcan@gmail.com>
2019-11-06 16:45:31 -0500

    watashi wa kama-kun, kazike kama-kun

--- Assets/Scripts/Catya.cs
+++ Assets/Scripts/Catya.cs
@@ -4,16 +4,45 @@ using UnityEngine;
 
 public class Catya : Enemy
 {
-    public float speed;
+    public GameObject brujula;
+    public static Vector3[] roadPoints;
+    int contPoints=0;
+
+    private void Awake()
+    {
+        Transform tempT = GameObject.Find("Catyaroad").transform;
+        roadPoints = new Vector3[tempT.childCount];
+        for (int i = 0, j = 0; i < tempT.childCount; i++, j = j + 40) 
+        {
+            //tempT.GetChild(i).transform.localPosition.
+            roadPoints[i] = tempT.GetChild(i).transform.position;
+            
+        }
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    //Vector3 tempTarget;
     void Update()
     {
-        
+        if (contPoints<roadPoints.Length)
+        {
+            if ((roadPoints[contPoints] - transform.position).magnitude > 20)
+            {
+                transform.LookAt(roadPoints[contPoints]);
+
+                //transform.LookAt(Vector3.Lerp(transform.eulerAngles,road);
+                GetComponent<Rigidbody>().velocity= transform.forward * speed*50;
+                transform.position += (roadPoints[contPoints] - transform.position).normalized * speed;
+            }
+            else
+            {
+                contPoints++;
+            }
+        }
     }
 
     int timer;
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     GameObject cube;
+    public float speed;
+    
 
 
     void Start()
@@ -15,11 +17,23 @@ public class Enemy : MonoBehaviour
             GameObject clon=Instantiate(cube);
             clon.transform.position=new Vector3(Random.Range(-300,300),Random.Range(-400,400),Random.Range(20,15000));
         }*/
+
+        //print(roadPoints.Length);
     }
-    
+
 
     void Update()
     {
         
     }
 }
+
+
+        /*Transform tempT = GameObject.Find("Warapo").transform;
+
+        childs = new GameObject[tempT.childCount];
+
+        for (int i = 0; i < tempT.childCount; i++)
+        {
+            childs[i] = tempT.GetChild(i).gameObject;
+        }*/
